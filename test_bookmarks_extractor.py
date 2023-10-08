593226772fa1440511de4873141c983e09ae166c
Roberto Carrillo <email@robertocarrillo.com>
2023-10-08 17:56:18 -0500

    Unit tests pass

--- test_bookmarks_extractor.py
+++ test_bookmarks_extractor.py
@@ -5,8 +5,8 @@ from chrome_bookmarks_extractor import BookmarksExtractor
 class TestBookmarksExtractor(unittest.TestCase):
     def test_extract_bookmarks(self):
         extractor = BookmarksExtractor()
-        bookmarks = extractor.extract_bookmarks('test_bookmarks.html')
-        self.assertEqual(len(bookmarks), 3)
+        bookmarks = extractor.extract_bookmarks('bookmarks.html')
+        self.assertEqual(len(bookmarks), 377)
 
 if __name__ == '__main__':
     unittest.main()
