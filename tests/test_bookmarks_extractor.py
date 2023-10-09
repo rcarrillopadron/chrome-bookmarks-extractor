8dc1b267f6ea61520f21406c82c4253dc6ce28da
Roberto Carrillo <email@robertocarrillo.com>
2023-10-08 19:09:00 -0500

    Supported new test

--- tests/test_bookmarks_extractor.py
+++ tests/test_bookmarks_extractor.py
@@ -1,4 +1,5 @@
 import unittest
+from pathlib import Path
 
 from chrome_bookmarks_extractor import BookmarksExtractor
 
@@ -6,8 +7,8 @@ from chrome_bookmarks_extractor import BookmarksExtractor
 class TestBookmarksExtractor(unittest.TestCase):
     def test_extract_bookmarks(self):
         extractor = BookmarksExtractor()
-        bookmarks = extractor.extract_bookmarks('bookmarks.html')
-        self.assertEqual(len(bookmarks), 377)
+        bookmarks = extractor.extract_bookmarks(Path("bookmarks.html"))
+        self.assertEqual(len(bookmarks), 376)
 
 
 if __name__ == '__main__':
